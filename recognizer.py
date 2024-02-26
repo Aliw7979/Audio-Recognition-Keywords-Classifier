84dcbc6f87ec5e6b43a2a0fdfa57ff0b6b9b64cf
aliw7979 <ali.eftekhari7997@gmail.com>
2024-02-27 01:52:28 +0330

    some cleanup

--- recognizer.py
+++ recognizer.py
@@ -3,6 +3,9 @@ import librosa
 import soundfile as sf
 import os
 REQUIRED_SAMPLE_RATE = 16000
+RESULT = "Model Predicted audio as : {}"
+MP3_EX = ".mp3"
+WAV_EX = ".wav"
 class InvalidFileFormatError(Exception):
     pass
 
@@ -19,17 +22,15 @@ class Recognizer:
 
     def predict(self, address : str):
       try:
-        if address.endswith(".mp3"):
+        if address.endswith(MP3_EX):
             audio, sample_rate = librosa.load(address, sr=None)
-            wav_file = address + ".wav"
+            wav_file = address + WAV_EX
             sf.write(wav_file, audio, sample_rate, format='wav')
             os.remove(wav_file)
             audio, sample_rate = librosa.load(address, sr=None)
             if sample_rate != REQUIRED_SAMPLE_RATE:
-                try:
                     if sample_rate != REQUIRED_SAMPLE_RATE:
                         audio = librosa.resample(audio, orig_sr=sample_rate, target_sr = REQUIRED_SAMPLE_RATE)
-                        print("changed sample rate of file.")   
                     
                     desired_length = REQUIRED_SAMPLE_RATE * 4
                     if len(audio) < desired_length:
@@ -40,38 +41,33 @@ class Recognizer:
                     audio = tf.expand_dims(audio, axis=0)
                     prediction = self._loaded_model(audio)
                     result = prediction['class_names'][0].numpy().decode('utf-8')
-                    print(f"Model Predicted audio as : {result}")
-                except Exception as e:
-                    print(e)
-            
-        elif address.endswith('.wav'):
+                    print(RESULT.format(result))
+            else:
+                prediction = self._loaded_model(address)
+                result = prediction['class_names'][0].numpy().decode('utf-8')
+                print(RESULT.format(result))
+
+        elif address.endswith(WAV_EX):
             audio, sample_rate = librosa.load(address, sr=None)
             if sample_rate != REQUIRED_SAMPLE_RATE:
-                print("Warning : Sample rate is not suitable for the model.")
-                try:
-                    if sample_rate != REQUIRED_SAMPLE_RATE:
-                        audio = librosa.resample(audio, orig_sr=sample_rate, target_sr = REQUIRED_SAMPLE_RATE)
-                        print("changed sample rate of file.")   
-                    
-                    desired_length = REQUIRED_SAMPLE_RATE * 4
-                    if len(audio) < desired_length:
-                        audio = tf.pad(audio, [[0, desired_length - len(audio)]])
-                    else:
-                        audio = audio[:desired_length]
+                if sample_rate != REQUIRED_SAMPLE_RATE:
+                    audio = librosa.resample(audio, orig_sr=sample_rate, target_sr = REQUIRED_SAMPLE_RATE)
+                
+                desired_length = REQUIRED_SAMPLE_RATE * 4
+                if len(audio) < desired_length:
+                    audio = tf.pad(audio, [[0, desired_length - len(audio)]])
+                else:
+                    audio = audio[:desired_length]
 
-                    audio = tf.expand_dims(audio, axis=0)
-                    prediction = self._loaded_model(audio)
-                    result = prediction['class_names'][0].numpy().decode('utf-8')
-                    print(f"Model Predicted audio as : {result}")
-                except Exception as e:
-                    print(e)
+                audio = tf.expand_dims(audio, axis=0)
+                prediction = self._loaded_model(audio)
+                result = prediction['class_names'][0].numpy().decode('utf-8')
+                print(RESULT.format(result))
             else:
-                try:
-                    prediction = self._loaded_model(address)
-                    result = prediction['class_names'][0].numpy().decode('utf-8')
-                    print(f"Model Predicted audio as : {result}")
-                except Exception as e:
-                    print(e)
+                prediction = self._loaded_model(address)
+                result = prediction['class_names'][0].numpy().decode('utf-8')
+                print(RESULT.format(result))
+
       except Exception as e:
         print(e)
 
