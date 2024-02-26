95319212741175f0f7794228ae16494bc242411c
aliw7979 <ali.eftekhari7997@gmail.com>
2024-02-27 01:43:48 +0330

    predict func now convert mp3 to wav

--- recognizer.py
+++ recognizer.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import librosa
-
+import soundfile as sf
+import os
 REQUIRED_SAMPLE_RATE = 16000
 class InvalidFileFormatError(Exception):
     pass
@@ -18,9 +19,32 @@ class Recognizer:
 
     def predict(self, address : str):
       try:
-        if not address.endswith(".wav"):
-            raise InvalidFileFormatError("Audio file format should be WAV.")
-        else:
+        if address.endswith(".mp3"):
+            audio, sample_rate = librosa.load(address, sr=None)
+            wav_file = address + ".wav"
+            sf.write(wav_file, audio, sample_rate, format='wav')
+            os.remove(wav_file)
+            audio, sample_rate = librosa.load(address, sr=None)
+            if sample_rate != REQUIRED_SAMPLE_RATE:
+                try:
+                    if sample_rate != REQUIRED_SAMPLE_RATE:
+                        audio = librosa.resample(audio, orig_sr=sample_rate, target_sr = REQUIRED_SAMPLE_RATE)
+                        print("changed sample rate of file.")   
+                    
+                    desired_length = REQUIRED_SAMPLE_RATE * 4
+                    if len(audio) < desired_length:
+                        audio = tf.pad(audio, [[0, desired_length - len(audio)]])
+                    else:
+                        audio = audio[:desired_length]
+
+                    audio = tf.expand_dims(audio, axis=0)
+                    prediction = self._loaded_model(audio)
+                    result = prediction['class_names'][0].numpy().decode('utf-8')
+                    print(f"Model Predicted audio as : {result}")
+                except Exception as e:
+                    print(e)
+            
+        elif address.endswith('.wav'):
             audio, sample_rate = librosa.load(address, sr=None)
             if sample_rate != REQUIRED_SAMPLE_RATE:
                 print("Warning : Sample rate is not suitable for the model.")
@@ -53,4 +77,4 @@ class Recognizer:
 
 recognizer = Recognizer('model')
 recognizer.load_model()
-recognizer.predict("1-(1)(1).wav")
+recognizer.predict("1-(1).mp3")
