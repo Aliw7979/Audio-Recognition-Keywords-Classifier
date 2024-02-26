ea18c2404a8451aa7f212e6969621edfd8b28ed7
aliw7979 <ali.eftekhari7997@gmail.com>
2024-02-27 01:21:19 +0330

    predict resample to 16Khz now

--- recognizer.py
+++ recognizer.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
-import soundfile as sf
+import librosa
+
 REQUIRED_SAMPLE_RATE = 16000
 class InvalidFileFormatError(Exception):
     pass
@@ -20,11 +21,22 @@ class Recognizer:
         if not address.endswith(".wav"):
             raise InvalidFileFormatError("Audio file format should be WAV.")
         else:
-            data, sample_rate = sf.read(address)
+            audio, sample_rate = librosa.load(address, sr=None)
             if sample_rate != REQUIRED_SAMPLE_RATE:
-                print("Warning : Target file should be in Wav format")
+                print("Warning : Sample rate is not suitable for the model.")
                 try:
-                    prediction = self._loaded_model(address)
+                    if sample_rate != REQUIRED_SAMPLE_RATE:
+                        audio = librosa.resample(audio, orig_sr=sample_rate, target_sr = REQUIRED_SAMPLE_RATE)
+                        print("changed sample rate of file.")   
+                    
+                    desired_length = REQUIRED_SAMPLE_RATE * 4
+                    if len(audio) < desired_length:
+                        audio = tf.pad(audio, [[0, desired_length - len(audio)]])
+                    else:
+                        audio = audio[:desired_length]
+
+                    audio = tf.expand_dims(audio, axis=0)
+                    prediction = self._loaded_model(audio)
                     result = prediction['class_names'][0].numpy().decode('utf-8')
                     print(f"Model Predicted audio as : {result}")
                 except Exception as e:
@@ -41,4 +53,4 @@ class Recognizer:
 
 recognizer = Recognizer('model')
 recognizer.load_model()
-recognizer.predict("1-(3).wav")
+recognizer.predict("1-(1)(1).wav")
